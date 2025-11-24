4170c026b23b83d842da8e131027459efcb274f4
mudib <o.goroschenya@yandex.ru>
2025-11-24 12:47:12 +0600

    Задание 3

---
+++ addressbook-web-tests/addressbook-web-tests/AccountData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    internal class AccountData
+    {
+        private string name;
+        private string password;
+
+        public AccountData (string name, string password)
+        {
+            this.name = name;
+            this.password = password;
+        }
+
+        public string Username 
+        { get { return name; } 
+          set { name = value; }
+        }
+        public string Password
+        { get { return password; }
+          set { password = value; } 
+        }
+    }
+}
